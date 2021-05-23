c8a00aeebfe09b03b7dfee01615c130fcd7873e0
Pratik Shirsath <69421598+pratikshirsathp@users.noreply.github.com>
2021-05-23 15:49:36 +0530

    Create Main.java

---
+++ Main.java
@@ -0,0 +1,24 @@
+public class Main{
+  public static void main(String args[]){
+
+
+    int num1;
+    int num2;;
+    num1 = 22;
+    num2 = 23;
+/*data types
+    float f = 3.7f;
+    double d = 4.7;
+
+    long l = 1283910;
+    short s = 236;
+    byte b = 1;
+*/
+    int result = num1 + num2;
+    String user = "pratik";
+    
+    System.out.print("addition of"+ num1 + "and" + num2+ "is:"+ result+ "performed by "+ user);
+
+    
+  }
+}
