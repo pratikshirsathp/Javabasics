4decadbc4fb043ad7d839fddcb65faa5804ef927
Pratik Shirsath <69421598+pratikshirsathp@users.noreply.github.com>
2021-05-23 18:04:54 +0530

    Create arrayjava.java

---
+++ arrayjava.java
@@ -0,0 +1,14 @@
+public class arrayjava{
+  
+  public static void main(String args[]){
+
+    int marks[]= new int[4];
+    marks[0] = 35;
+    marks[1] = 45;
+    marks[3] = 0;
+    System.out.println(marks[0]);
+
+  }
+
+
+}
