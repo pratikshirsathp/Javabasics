bb5976469022b2eaff70ef54bd3b963c3e81d5e7
Pratik Shirsath <69421598+pratikshirsathp@users.noreply.github.com>
2021-05-23 21:51:36 +0530

    Update forloop.java

--- forloop.java
+++ forloop.java
@@ -4,9 +4,17 @@ public class forloop{
 
     for(int i=1; i<=5;i++){
       System.out.println("Hi"+ i);
-
-    }
-
+      }
+  //printing array
+    int arr[] = new int[5];
+    arr[0] = 45;
+    arr[1] = 65;
+    arr[2] = 15;
+    arr[3] = 25;
+    arr[4] = 35;
+    for(int i=0;i<5;i++){
+        System.out.println(arr[i]);
+      }
   }
 
 
