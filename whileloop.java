fd3e8ff9bd937a93486ff2c8fee036d8e866435b
Pratik Shirsath <69421598+pratikshirsathp@users.noreply.github.com>
2021-05-23 17:21:40 +0530

    Create whileloop.java

---
+++ whileloop.java
@@ -0,0 +1,20 @@
+public class whileloop{
+
+  public static void main( String args[]){
+    //while
+    int i = 0;
+    while (i<5){
+        System.out.println("hey!");
+        i+=1;
+    }
+
+    //do while
+    int j = 7;
+    do{
+      System.out.println("hey j!");
+      j++;
+    }while(j<5);
+
+
+  }
+}
