a1c0523e1b12801b446e43719c046dd9da5f0b8f
Pratik Shirsath <69421598+pratikshirsathp@users.noreply.github.com>
2021-05-24 10:41:23 +0530

    Update addmatrix.java

--- addmatrix.java
+++ addmatrix.java
@@ -1,1 +1,28 @@
+import java.util.Random;
+public class addmatrix{
+  public static void main(String args[]){
+    //random number generator
+    Random random = new Random();
+    //Scanner sc = new Scanner(System.in);
+    int matrix[][] = new int[3][3];
+    int matrix2[][] = new int[3][3];
+    int matrix3[][] = new int[3][3];
 
+    for (int i=0; i<3;i++){
+      for (int j=0;j<3;j++){
+        matrix[i][j] = random.nextInt(9);
+        matrix2[i][j] = random.nextInt(9);
+
+      }
+    }
+    for (int i=0; i<3;i++){
+      for (int j=0;j<3;j++){
+      matrix3[i][j] = matrix[i][j]+ matrix2[i][j];
+      System.out.print(matrix3[i][j]+" ");
+      }
+      System.out.println();
+
+    } 
+
+    }
+}
